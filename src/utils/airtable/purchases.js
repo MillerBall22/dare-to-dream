03ccd1a2be708ce810e2aca7761282ba9250f963
MillerBall22 <millerball22@gmail.com>
2025-05-06 13:44:39 -0600

    Poster Update 2025

--- src/utils/airtable/purchases.js
+++ src/utils/airtable/purchases.js
@@ -1,39 +1,39 @@
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appivDaLROdZVqghr');
+// var Airtable = require('airtable');
+// var base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appivDaLROdZVqghr');
 
-export const createPurchase = async (purchaseInfo) => {
-    const {date, lastName, firstName, email, primaryPhone, secondaryPhone, address, city, postalCode, ticketsPurchased, singleTicket, threeTickets, tenTickets, fiftyFiftyTickets, totalPrice, credit, expiry, CVC} = purchaseInfo;
-    base('Purchases').create([
-      {
-        "fields": {
-          "Date": date,
-          "Last Name": lastName,
-          "First Name": firstName,
-          "Email": email,
-          "Primary Phone": primaryPhone,
-          "Secondary Phone": secondaryPhone,
-          "Address": address,
-          "City": city,
-          "Postal Code": postalCode,
-          "Tickets Purchased": ticketsPurchased,
-          "Single Ticket": singleTicket,
-          "3 Tickets": threeTickets,
-          "10 Tickets": tenTickets,
-          "50/50 Tickets": fiftyFiftyTickets,
-          "Total Price": totalPrice,
-          "Credit Number": credit,
-          "Expiration Date": expiry,
-          "CVC": CVC,
-          "Payment Status": "Unpaid",
-        }
-      }
-    ], function(err, records) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      records.forEach(function (record) {
-        console.log(record.getId());
-      });
-    });
-}
+// export const createPurchase = async (purchaseInfo) => {
+//     const {date, lastName, firstName, email, primaryPhone, secondaryPhone, address, city, postalCode, ticketsPurchased, singleTicket, threeTickets, tenTickets, fiftyFiftyTickets, totalPrice, credit, expiry, CVC} = purchaseInfo;
+//     base('Purchases').create([
+//       {
+//         "fields": {
+//           "Date": date,
+//           "Last Name": lastName,
+//           "First Name": firstName,
+//           "Email": email,
+//           "Primary Phone": primaryPhone,
+//           "Secondary Phone": secondaryPhone,
+//           "Address": address,
+//           "City": city,
+//           "Postal Code": postalCode,
+//           "Tickets Purchased": ticketsPurchased,
+//           "Single Ticket": singleTicket,
+//           "3 Tickets": threeTickets,
+//           "10 Tickets": tenTickets,
+//           "50/50 Tickets": fiftyFiftyTickets,
+//           "Total Price": totalPrice,
+//           "Credit Number": credit,
+//           "Expiration Date": expiry,
+//           "CVC": CVC,
+//           "Payment Status": "Unpaid",
+//         }
+//       }
+//     ], function(err, records) {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       records.forEach(function (record) {
+//         console.log(record.getId());
+//       });
+//     });
+// }
--- src/utils/airtable/users.js
+++ src/utils/airtable/users.js
@@ -1,84 +1,84 @@
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appivDaLROdZVqghr');
+// var Airtable = require('airtable');
+// var base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appivDaLROdZVqghr');
 
-export const getUser = async (email) => {
-  return new Promise((resolve, reject) => {
-    base('Users').select({
-      filterByFormula: `{Email} = "${email}"`,
-      }).eachPage(function page(records) {
-        if (records.length === 0) {
-          createUser(email);
-          resolve(getUser(email))
-        }
-      records.every(async function(record) {
-      const result = {
-        id: record.id,
-        fields: record.fields
-      }
-        resolve(result);
-        return true
-      }); 
-      });
-  });
-}
+// export const getUser = async (email) => {
+//   return new Promise((resolve, reject) => {
+//     base('Users').select({
+//       filterByFormula: `{Email} = "${email}"`,
+//       }).eachPage(function page(records) {
+//         if (records.length === 0) {
+//           createUser(email);
+//           resolve(getUser(email))
+//         }
+//       records.every(async function(record) {
+//       const result = {
+//         id: record.id,
+//         fields: record.fields
+//       }
+//         resolve(result);
+//         return true
+//       }); 
+//       });
+//   });
+// }
 
-export const getUserById = async (id) => {
-  return new Promise((resolve, reject) => {
-    base('Users').find(id, function (err, record) {
-      if (err) { 
-        console.error(err); 
-        return; 
-      }
-      const results = {
-        id: record.id,
-        fields: record.fields
-      }
-      resolve(results)
-    });
-  });
-}
+// export const getUserById = async (id) => {
+//   return new Promise((resolve, reject) => {
+//     base('Users').find(id, function (err, record) {
+//       if (err) { 
+//         console.error(err); 
+//         return; 
+//       }
+//       const results = {
+//         id: record.id,
+//         fields: record.fields
+//       }
+//       resolve(results)
+//     });
+//   });
+// }
 
-export const createUser = async (userEmail) => {
-    base('Users').create([
-      {
-        "fields": {
-            Email: userEmail
-        }
-      }
-    ], function(err, records) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      records.forEach(function (record) {
-        console.log(record.getId());
-      });
-    });
-}
+// export const createUser = async (userEmail) => {
+//     base('Users').create([
+//       {
+//         "fields": {
+//             Email: userEmail
+//         }
+//       }
+//     ], function(err, records) {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       records.forEach(function (record) {
+//         console.log(record.getId());
+//       });
+//     });
+// }
 
-export const updateUser = async (userId, update) => {
-  const {email, firstName, lastName, primaryPhone, secondaryPhone, address, city, postalCode} = update
-  base('Users').update([
-    {
-      "id": userId,
-      "fields": {
-        "Email": email,
-        "Last Name": lastName,
-        "First Name": firstName,
-        "Primary Phone": primaryPhone,
-        "Secondary Phone": secondaryPhone,
-        "Address": address,
-        "City": city,
-        "Postal Code": postalCode
-      }
-    }
-  ], function(err, records) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    records.forEach(function(record) {
-      console.log(record.get('email'));
-    });
-  });
-}
+// export const updateUser = async (userId, update) => {
+//   const {email, firstName, lastName, primaryPhone, secondaryPhone, address, city, postalCode} = update
+//   base('Users').update([
+//     {
+//       "id": userId,
+//       "fields": {
+//         "Email": email,
+//         "Last Name": lastName,
+//         "First Name": firstName,
+//         "Primary Phone": primaryPhone,
+//         "Secondary Phone": secondaryPhone,
+//         "Address": address,
+//         "City": city,
+//         "Postal Code": postalCode
+//       }
+//     }
+//   ], function(err, records) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     records.forEach(function(record) {
+//       console.log(record.get('email'));
+//     });
+//   });
+// }
